ae6596d02df2dfe0c9a1edd5b74b1d0a49d8af23
Ryan Cartwright <fryzee.cartwright@gmail.com>
2023-08-21 12:23:27 +1000

    change go-sdk/resources deps to go-sdk/nitric within go docs

--- src/nav.config.ts
+++ src/nav.config.ts
@@ -1120,6 +1120,10 @@ const fullNav: FullNav = {
           href: '/reference/go/v0/storage/bucket',
         },
         {
+          title: 'NewTopic()',
+          href: '/reference/go/v0/topic/topic',
+        },
+        {
           title: 'NewSchedule()',
           href: '/reference/go/v0/schedule/schedule',
         },
