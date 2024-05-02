66adfa2c333a88f05aa6959b8d735cface4b862e
David Moore <4121492+davemooreuws@users.noreply.github.com>
2024-05-02 10:16:27 +1000

    feat: auto generate cli help snippet and update cli docs (#542)
    Within the vercel environment, this will install the CLI and output the
    nitric help command into a txt file for the build.

--- src/mdx/remark.mjs
+++ src/mdx/remark.mjs
@@ -1,4 +1,5 @@
 import { mdxAnnotations } from 'mdx-annotations'
 import remarkGfm from 'remark-gfm'
+import codeImport from 'remark-code-import'
 
-export const remarkPlugins = [mdxAnnotations.remark, remarkGfm]
+export const remarkPlugins = [mdxAnnotations.remark, remarkGfm, codeImport]
