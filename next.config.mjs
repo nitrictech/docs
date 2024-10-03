a87dc5641e7e2899903e327485062323b3f46911
Tim Holm <tim.holm@nitric.io>
2024-10-03 15:08:53 +1000

    Go SDK doc updates (#621)
    Update go SDK examples and reference docs to stable v1 SDK
    
    ---------
    
    Co-authored-by: David Moore <davemooreuws@gmail.com>
    Co-authored-by: Ryan Cartwright <fryzee.cartwright@gmail.com>
    Co-authored-by: Jye Cusch <jye.cusch@nitric.io>

--- next.config.mjs
+++ next.config.mjs
@@ -37,6 +37,23 @@ const nextConfig = {
       },
       // redirects from old docs
       ...[
+        '/docs/reference/go/secrets/secret-version',
+        '/docs/reference/go/secrets/secret-latest',
+        '/docs/reference/go/secrets/secret-version-access',
+        '/docs/reference/go/storage/bucket-file',
+        '/docs/reference/go/storage/bucket-files',
+        '/docs/reference/go/storage/bucket-file-read',
+        '/docs/reference/go/storage/bucket-file-write',
+        '/docs/reference/go/storage/bucket-file-delete',
+        '/docs/reference/go/storage/bucket-file-downloadurl',
+        '/docs/reference/go/storage/bucket-file-uploadurl',
+      ].map((source) => ({
+        source: source,
+        destination: `/docs/reference/go`,
+        basePath: false,
+        permanent: true,
+      })),
+      ...[
         '/docs/testing',
         '/docs/guides/debugging',
         '/docs/guides/serverless-rest-api-example',
