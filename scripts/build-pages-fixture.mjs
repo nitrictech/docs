bf0a269ddd493f90e684b4cfaa0350d72f5c8d35
David Moore <davemooreuws@gmail.com>
2024-10-04 12:47:05 +1000

    generate cypress fixtures

---
+++ scripts/build-pages-fixture.mjs
@@ -0,0 +1,25 @@
+import fs from 'fs/promises'
+
+import { allDocs } from '../.contentlayer/generated/index.mjs'
+
+const FIXTURE_PATH = 'cypress/fixtures/pages.json'
+
+async function run() {
+  try {
+    const paths = allDocs
+      .map((doc) => {
+        return doc.slug ? `/${doc.slug}` : '/'
+      })
+      .sort((a, b) => a.localeCompare(b))
+
+    console.log(`${paths.length} paths found. Generating fixture`)
+
+    await fs.writeFile(FIXTURE_PATH, JSON.stringify(paths))
+  } catch (error) {
+    console.log(error.message)
+
+    process.exit(1)
+  }
+}
+
+run()
