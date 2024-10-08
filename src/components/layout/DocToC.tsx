fdf71550c23a32a7097f868df005ee7dab2d83fa
Ryan Cartwright <39504851+HomelessDinosaur@users.noreply.github.com>
2024-10-09 09:37:37 +1100

    Update scrollbar to change on heading scroll rather than scroll percent (#626)

--- src/components/layout/DocToC.tsx
+++ src/components/layout/DocToC.tsx
@@ -1,20 +1,13 @@
 'use client'
 
 import type { Doc } from '@/content'
-import React, { useRef } from 'react'
-import { Badge } from '../ui/badge'
+import { useRef, useLayoutEffect, useState, useEffect } from 'react'
 import Link from 'next/link'
-import { FaFacebook, FaFilePdf, FaLinkedin } from 'react-icons/fa'
 import { DocTracingBeam } from './DocTracingBeam'
-import { BASE_URL } from '@/lib/constants'
-import { title } from 'radash'
 import { Button } from '../ui/button'
 import { GitHubIcon } from '../icons/GitHubIcon'
-
-interface Props {
-  doc: Doc
-  articleRef?: React.RefObject<HTMLDivElement>
-}
+import { useMotionValue } from 'framer-motion'
+import { cn } from '@/lib/utils'
 
 interface Toc {
   url: string
@@ -22,25 +15,70 @@ interface Toc {
   depth: number
 }
 
-const DocToC: React.FC<Props> = ({ doc }) => {
-  const articleRef = useRef<HTMLDivElement>(null)
+const DocToC = ({ doc }: { doc: Doc }) => {
+  const initial = 14
+  const sectionSize = 28
+  const offset = 10
+
+  const y1 = useMotionValue(0)
+  const y2 = useMotionValue(0)
+
+  const [activeIndex, setActiveIndex] = useState(-1)
+
+  useEffect(() => {
+    const headings = document
+      .querySelectorAll<HTMLHeadingElement>('h2.md-content-header')
+      .values()
+      .toArray()
+
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 1, // Adjust based on when you want to highlight
+    }
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const index = headings.findIndex(
+            (heading) => heading.textContent === entry.target.textContent,
+          )
+
+          y2.set(initial + (index * sectionSize + offset))
+
+          setActiveIndex(index)
+        }
+      })
+    }, options)
+
+    headings.forEach((h2) => observer.observe(h2))
+
+    return () => {
+      headings.forEach((h2) => observer.unobserve(h2))
+    }
+  }, [])
 
   return (
-    <div className="hidden h-full min-w-52 md:block" ref={articleRef}>
+    <div className="hidden h-full min-w-52 md:block">
       <aside className="sticky top-[calc(var(--header-height)+1px+2rem)] max-h-[calc(100vh-var(--header-height)-3rem)] min-w-40 space-y-6">
         {doc.toc.length ? (
           <div className="relative flex flex-col">
             <p className="mb-2 font-mono text-sm uppercase dark:text-zinc-300">
               On this page
             </p>
-            <DocTracingBeam targetRef={articleRef}>
+            <DocTracingBeam y1={y1} y2={y2}>
               <ol className="flex flex-col gap-y-1 pl-4 text-sm font-medium">
                 {doc.toc.map((item: Toc, i: number) => {
                   return (
                     <li key={item.url + i}>
                       <Link
                         href={item.url}
-                        className="text-2xs text-muted-foreground transition-colors hover:text-zinc-900 dark:hover:text-zinc-200"
+                        className={cn(
+                          'text-2xs',
+                          activeIndex === i
+                            ? 'text-zinc-900 dark:text-zinc-200'
+                            : 'text-muted-foreground transition-colors hover:text-zinc-900 dark:hover:text-zinc-200',
+                        )}
                       >
                         {item.value}
                       </Link>
