905a6d06eb993f98d2863eae7b47e93fdb57c61a
David Moore <davemooreuws@gmail.com>
2024-10-10 10:30:59 +1100

    fix copy button on scroll / transitions

--- src/components/code/CopyButton.tsx
+++ src/components/code/CopyButton.tsx
@@ -28,7 +28,7 @@ export function CopyButton({
     <button
       type="button"
       className={cn(
-        'group/button absolute right-2 top-2.5 h-8 rounded-md px-1.5 py-1 text-2xs font-medium backdrop-blur transition',
+        'group/button absolute right-3.5 top-2.5 z-10 h-8 rounded-md px-1.5 py-1 text-2xs font-medium backdrop-blur transition',
         copied
           ? 'bg-primary-400/10 ring-primary-400/20'
           : 'bg-white/5 ring-1 ring-inset ring-zinc-300/10 hover:bg-white/7.5 dark:bg-white/2.5 dark:hover:bg-white/5',
