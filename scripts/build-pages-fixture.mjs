1a2f891b61180be6015598a83205c369422c36f0
David Moore <davemooreuws@gmail.com>
2024-10-11 13:20:11 +1100

    add base pages (guides) to sitemap and tests

--- scripts/build-pages-fixture.mjs
+++ scripts/build-pages-fixture.mjs
@@ -1,16 +1,19 @@
 import fs from 'fs/promises'
 
 import { allDocs } from '../.contentlayer/generated/index.mjs'
+import pages from '../src/assets/sitemap.json' with { type: 'json' }
 
 const FIXTURE_PATH = 'cypress/fixtures/pages.json'
 
 async function run() {
   try {
-    const paths = allDocs
-      .map((doc) => {
-        return doc.slug === '/' ? '/docs' : `/docs/${doc.slug}`
-      })
-      .sort((a, b) => a.localeCompare(b))
+    const basePages = pages.map((p) => p.replace('https://nitric.io', ''))
+
+    const docPages = allDocs.map((doc) => {
+      return doc.slug === '/' ? '/docs' : `/docs/${doc.slug}`
+    })
+
+    const paths = [...basePages, ...docPages].sort((a, b) => a.localeCompare(b))
 
     console.log(`${paths.length} paths found. Generating fixture`)
 
--- src/app/(sitemaps)/sitemap-0.xml/route.ts
+++ src/app/(sitemaps)/sitemap-0.xml/route.ts
@@ -1,4 +1,5 @@
 import { allDocs } from '@/content'
+import staticPaths from '@/assets/sitemap.json'
 
 const URL = 'https://nitric.io/docs'
 
@@ -9,16 +10,27 @@ interface SitemapItem {
   priority: number
 }
 
+const lastmod = new Date().toISOString()
+
 // Function to construct the XML structure of the sitemap index.
 export async function GET() {
-  const pages: SitemapItem[] = allDocs.map((page) => ({
+  const pages = staticPaths.map((page) => ({
+    loc: page,
+    lastmod,
+    changefreq: 'daily',
+    priority: 0.7,
+  }))
+
+  const docPages: SitemapItem[] = allDocs.map((page) => ({
     loc: page.slug === '' ? URL : `${URL}/${page.slug}`,
     lastmod: new Date(page.lastModified).toISOString(),
     changefreq: 'daily',
     priority: 0.7,
   }))
 
-  const allPagesSorted = [...pages].sort((a, b) => (a.loc < b.loc ? -1 : 1))
+  const allPagesSorted = [...pages, ...docPages].sort((a, b) =>
+    a.loc < b.loc ? -1 : 1,
+  )
 
   const sitemapIndexXML = buildSitemap(allPagesSorted)
 
