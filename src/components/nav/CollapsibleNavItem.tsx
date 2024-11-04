62ad5611a879b16be40feb1af3338e3b3c3e4ad3
David Moore <4121492+davemooreuws@users.noreply.github.com>
2024-11-04 14:00:27 +1100

    fix: open nav group if pathname has changed (#656)

--- src/components/nav/CollapsibleNavItem.tsx
+++ src/components/nav/CollapsibleNavItem.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useId } from 'react'
+import React, { useEffect, useId } from 'react'
 import { Collapsible, CollapsibleTrigger } from '../ui/collapsible'
 import { ChevronDownIcon } from '@heroicons/react/24/outline'
 import { cn } from '@/lib/utils'
@@ -36,9 +36,16 @@ const CollapsibleNavItem: React.FC<Props> = ({ group, className }) => {
 
   const { title, items, icon: Icon } = group
 
-  const isActive = checkIfActive(items, pathname)
+  const [isOpen, setIsOpen] = React.useState(checkIfActive(items, pathname))
 
-  const [isOpen, setIsOpen] = React.useState(isActive)
+  useEffect(() => {
+    const isActive = checkIfActive(items, pathname)
+
+    // only open if the group is active
+    if (isActive) {
+      setIsOpen(isActive)
+    }
+  }, [pathname])
 
   return (
     <Collapsible
