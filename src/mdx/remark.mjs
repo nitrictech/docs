633374441ea58015b14221e8ce0d578a4cf6903b
Tim Holm <tim.holm@nitric.io>
2024-12-20 11:43:06 +1100

    Add mermaid-mdx and basic sitewide config.

--- src/mdx/remark.mjs
+++ src/mdx/remark.mjs
@@ -1,4 +1,25 @@
 import { mdxAnnotations } from 'mdx-annotations'
 import remarkGfm from 'remark-gfm'
+import mdxMermaid from 'mdx-mermaid'
 
-export const remarkPlugins = [mdxAnnotations.remark, remarkGfm]
+export const remarkPlugins = [
+  mdxAnnotations.remark,
+  remarkGfm,
+  [
+    mdxMermaid,
+    {
+      output: 'svg',
+      mermaid: {
+        theme: 'base',
+        themeVariables: {
+          primaryColor: '#ff0000',
+          secondaryColor: '#00ff00',
+          tertiaryColor: '#0000ff',
+          textColor: '#000000',
+          fontSize: '14px',
+          fontFamily: 'Fira Code, monospace',
+        },
+      },
+    },
+  ],
+]
